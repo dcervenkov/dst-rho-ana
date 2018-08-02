ff52f9200788e016f5a5f619e9249ac10df6d1b4
Daniel Cervenkov <d.cervenkov@gmail.com>
2018-08-02 20:33:03 +0200

    Fix make_configs.py having hardcoded constants

--- DSRhoCPFit/tools/make_configs.py
+++ DSRhoCPFit/tools/make_configs.py
@@ -22,7 +22,7 @@ def make_configs(var_name, begin, end, steps, config_template, filename_base):
     with open(config_template, "r") as f:
         json_template = json.load(f)
 
-    for i, var_ranges in enumerate(generate_ranges(0, 3.1415, 5)):
+    for i, var_ranges in enumerate(generate_ranges(begin, end, steps)):
         config = json_template
         config["fitRanges"][var_name] = var_ranges
 

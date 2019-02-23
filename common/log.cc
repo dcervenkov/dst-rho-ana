43189d1ca407ed45ecf30c6e0b4a1d1a877dbc85
Daniel Cervenkov <d.cervenkov@gmail.com>
2019-02-23 22:03:54 +0100

    Add a color coded label to log messages

--- common/log.cc
+++ common/log.cc
@@ -8,6 +8,12 @@ static Log::LogLevel log_level;
 
 namespace Log {
 
+const char* reset = "\033[0m";
+const char* red = "\033[31m";
+const char* green = "\033[32m";
+const char* yellow = "\033[33m";
+const char* blue = "\033[34m";
+
 void setLogLevel(LogLevel level) { log_level = level; }
 
 void print(LogLevel level, const char* fmt, ...) {
@@ -16,6 +22,22 @@ void print(LogLevel level, const char* fmt, ...) {
         va_start(ap, fmt);
         char buf[1000];
         vsnprintf(buf, 1000, fmt, ap);
+        switch (level) {
+            case debug:
+                std::cout << blue << "[DEBUG] " << reset;
+                break;
+            case info:
+                std::cout << green << "[INFO] " << reset;
+                break;
+            case warning:
+                std::cout << yellow << "[WARNING] " << reset;
+                break;
+            case error:
+                std::cout << red << "[ERROR] " << reset;
+                break;
+            default:
+                break;
+        }
         std::cout << buf;
         va_end(ap);
     }

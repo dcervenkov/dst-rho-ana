68dc9141763f8b79ab2a23919006fb8a00debea6
Daniel Cervenkov <d.cervenkov@gmail.com>
2019-03-27 14:01:37 +0100

    Add files missing from the previous fix

---
+++ DSRhoPeek/src/peekconstants.h
@@ -0,0 +1,24 @@
+/*
+ * peekconstants.h
+ *
+ *  Created on: Oct 9, 2015
+ *      Author: cervenkov
+ */
+
+#ifndef PEEKCONSTANTS_H_
+#define PEEKCONSTANTS_H_
+
+const int MIN_ENTRIES = 0;
+
+const int NUM_MC_FLAGS = 9;
+const int MC_FLAGS_ASSIGNMENT[NUM_MC_FLAGS] = { -1, 1, 2, 3,  4, 5,  6, 7,  8};
+const int COLOR_MAP[NUM_MC_FLAGS] =           { 13, 4, 7, 6, 20, 2, 30, 8, 31};
+
+// Signal MC flags; used for figure-of-merit calculation
+const int MC_FLAG_SIGNAL = 1;
+const int MC_FLAG_SIGNAL_WITHOUT_FSR = 10;
+
+// BASF identifies histograms via numbers
+const int BASF_HIST_ID_BEST = 2000; // Best candidate
+
+#endif /* PEEKCONSTANTS_H_ */

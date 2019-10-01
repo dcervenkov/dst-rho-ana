bbcd561845aa0ae1e2e631fcc7b02f5195352eb9
Daniel Cervenkov <d.cervenkov@gmail.com>
2019-10-01 13:21:11 +0200

    Improve fit_all.py; allow to select num streams

--- DSRhoCPFit/tools/fit_all.py
+++ DSRhoCPFit/tools/fit_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import itertools
 import os
 import shutil
 import subprocess
@@ -11,34 +12,33 @@ DEFAULT_OPTIONS = ["--MC=1",
                    "--log"]
 COMMAND = "./DSRhoCPFit"
 CHANNELS = ["Kpi", "Kpipi0", "K3pi"]
-STREAMS = range(0, 6)
 CONFIG_BASENAME = "config_stream"
 RESULTS_DIR = "results"
 TEMP_FILE = "/tmp/temp_commands"
 
+CHANNELS_COMBINATIONS = CHANNELS[:]
+CHANNELS_COMBINATIONS += [",".join([c1, c2]) for c1 in CHANNELS for c2 in CHANNELS if c1 != c2]
+CHANNELS_COMBINATIONS += list(map(",".join, list(itertools.permutations(CHANNELS))))
 
 def decode_arguments():
     """Decode CLI arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--channel", action="append",
-                        help="channel to be processed")
+    parser.add_argument("-c", "--channel", action="append", help="channel to be processed",
+                        dest="channels", choices=CHANNELS_COMBINATIONS)
     parser.add_argument("-d", "--td", action="store_true",
                         help="do time-dependent fits")
     parser.add_argument("-i", "--ti", action="store_true",
                         help="do time-independent fits")
-    parser.add_argument("-m", "--component",
+    parser.add_argument("-m", "--component", dest="components", choices=["CR", "CRSCF", "all"],
                         action="append", help="do CR-only fits")
     parser.add_argument("-f", "--force", action="store_true",
                         help="force rewrite of existing results")
     parser.add_argument(
-        "-j", "--cpus", help="number of cores to use", default="1")
+        "-j", "--cpus", help="number of cores to use", default=1, type=int)
+    parser.add_argument("-s", "--streams", default=6, type=int,
+                        help="force rewrite of existing results")
     args = parser.parse_args()
-    return {"channels": args.channel,
-            "components": args.component,
-            "ti": args.ti,
-            "td": args.td,
-            "force": args.force,
-            "cpus": args.cpus}
+    return args
 
 
 def are_valid(options):
@@ -50,21 +50,21 @@ def create_fit_cmds(options):
     """Fit one channel with all the components, fit types, etc."""
     commands = []
     time_types = []
-    if (options["ti"]):
+    if (options.ti):
         time_types.append("ti")
-    if (options["td"]):
+    if (options.td):
         time_types.append("td")
 
     dirs_to_create = []
     dirs_to_delete = []
     dirs_to_skip = []
     for time_type in time_types:
-        for channel in options["channels"]:
-            for component in options["components"]:
+        for channel in options.channels:
+            for component in options.components:
                 output_dir = os.path.join(RESULTS_DIR, "_".join(
                     [channel.replace(",", "-"), time_type, component]))
                 if os.path.exists(os.path.join("..", output_dir)):
-                    if options["force"]:
+                    if options.force:
                         dirs_to_delete.append(output_dir)
                     else:
                         dirs_to_skip.append(output_dir)
@@ -72,7 +72,7 @@ def create_fit_cmds(options):
                 else:
                     dirs_to_create.append(output_dir)
 
-                for stream in STREAMS:
+                for stream in range(0, options.streams):
                     cmd = []
                     cmd.append(COMMAND)
                     cmd += DEFAULT_OPTIONS
@@ -96,6 +96,7 @@ def create_fit_cmds(options):
 
 def main():
     options = decode_arguments()
+    print(options)
     if not are_valid(options):
         sys.exit(1)
 
@@ -121,7 +122,9 @@ def main():
         for dir_to_delete in dirs_to_delete:
             print(dir_to_delete)
 
-    print("\nWill run on " + options["cpus"] + " core(s)")
+    print()
+    print("Will run on " + str(options.cpus) + " core(s)")
+    print("Will process " + str(options.streams) + " stream(s)")
 
     user_input = input("\nContinue? [Y/n]: ")
     if (user_input == "" or user_input.lower() == "y"):
@@ -136,7 +139,7 @@ def main():
             os.mkdir(os.path.join("..", dir))
 
         subprocess.run(
-            "nice parallel -j " + options["cpus"] + " < " + TEMP_FILE, cwd="..", shell=True)
+            "nice parallel -j " + str(options.cpus) + " < " + TEMP_FILE, cwd="..", shell=True)
         os.remove(TEMP_FILE)
     else:
         print("Aborting")

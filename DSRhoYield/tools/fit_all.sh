674d7ee3dfba61d3bd878a2f0ef0a6d45d1735b1
Daniel Cervenkov <d.cervenkov@gmail.com>
2020-01-26 11:22:02 +0100

    Fix fit_all script in Yield not fixing w for MC

--- DSRhoYield/tools/fit_all.sh
+++ DSRhoYield/tools/fit_all.sh
@@ -4,7 +4,7 @@ mkdir -p logs
 mkdir -p plots
 for CHANNEL in "Kpi" "Kpipi0" "K3pi"; do
     for STREAM in $(seq 0 5); do
-        ./DSRhoYield ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL}/realistic_mc/stream${STREAM} plots/${CHANNEL}_stream${STREAM} &> logs/${CHANNEL}_stream${STREAM} &
+        ./DSRhoYield --MC ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL}/realistic_mc/stream${STREAM} plots/${CHANNEL}_stream${STREAM} &> logs/${CHANNEL}_stream${STREAM} &
     done
     ./DSRhoYield ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL} plots/${CHANNEL} &> logs/${CHANNEL} &
 done

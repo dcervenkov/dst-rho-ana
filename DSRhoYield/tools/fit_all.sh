135e449f7f4ec1b5f82c2e2f22887bd3cdd73402
Daniel Cervenkov <d.cervenkov@gmail.com>
2020-01-30 15:37:25 +0100

    Make Yield fit_all.sh use parallel

--- DSRhoYield/tools/fit_all.sh
+++ DSRhoYield/tools/fit_all.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+CMD_FILE="tools/cmds.tmp"
+
+cd $(dirname $0)
+cd ..
+
+rm -f ${CMD_FILE}
+touch ${CMD_FILE}
 mkdir -p logs
 mkdir -p plots
+
 for CHANNEL in "Kpi" "Kpipi0" "K3pi"; do
     for STREAM in $(seq 0 5); do
-        ./DSRhoYield --MC ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL}/realistic_mc/stream${STREAM} plots/${CHANNEL}_stream${STREAM} &> logs/${CHANNEL}_stream${STREAM} &
+        echo "./DSRhoYield --MC ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL}/realistic_mc/stream${STREAM} plots/${CHANNEL}_stream${STREAM} &> logs/${CHANNEL}_stream${STREAM}" >> ${CMD_FILE}
     done
-    ./DSRhoYield ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL} plots/${CHANNEL} &> logs/${CHANNEL} &
+    echo "./DSRhoYield ../data/${CHANNEL}/realistic_mc ../data/${CHANNEL} plots/${CHANNEL} &> logs/${CHANNEL}" >> ${CMD_FILE}
 done
+
+echo "Running $(wc -l ${CMD_FILE} | cut -d' ' -f1) fits..."
+parallel --will-cite --bar < ${CMD_FILE}
+
+if [ "$?" -eq 0 ]; then
+    rm ${CMD_FILE}
+    exit 0
+else
+    exit 1
+fi

d9f2036f7f35ec2b08372e18ec998fc0c33eadb6
Daniel Cervenkov <d.cervenkov@gmail.com>
2020-02-25 16:16:16 +0100

    Add script to summarize Lifetime results

---
+++ DSRhoLifetime/tools/summarize_results.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for CHANNEL in Kpi Kpipi0 K3pi; do
+    echo "${CHANNEL}"
+    for TYPE in CR CRSCF all; do
+        printf "%6s " ${TYPE}
+        awk '{life += $1; err += $2; count++} END {printf "%.3f +- %.3f\n",life/count,err/count}' ../results/${CHANNEL}/${TYPE}_lifetime_*[0-6]
+    done
+    echo
+done

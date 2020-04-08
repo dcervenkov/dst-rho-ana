39c5f703a97267b1a99267ce81018114a749377d
Daniel Cervenkov <d.cervenkov@gmail.com>
2020-04-08 19:27:33 +0200

    Make config generating script match new results
    Results format of Background changed. This required a change in the
    config_from_template.py script. These changes were necessary in order to
    support Cabibbo-favored and doubly Cabibbo-suppressed dt models,
    alongside the standard all-together model.

--- tools/config_from_template.py
+++ tools/config_from_template.py
@@ -6,7 +6,8 @@ referenced files. Each 'substitute' key is an array of one or more JSON
 objects. Each such object must have a 'file' key, which holds the name of a
 file to be read-in. An object might also have a 'replace' key which specifies
 that the newly read-in key names should be modified by replacing a substring of
-each key name with a new string.
+each key name with a new string. An optional 'include' key selects which
+sub-dictionaries are picked from the included file and concatenated together.
 
 An example template:
 {
@@ -14,7 +15,8 @@ An example template:
         "substitute": [
             {
                 "file": "Kpi_mc_scf.json",
-                "replace": ["old", "new"]
+                "replace": ["old", "new"],
+                "include": ["phys_dt_dcs", "phys_dt_cf"]
             }
         ],
         "cr_f": 0.764,
@@ -43,8 +45,9 @@ def read_in_json(file):
 def remove_extra_keys(dictionary):
     """Remove certain keys, that are relicts from the fit programs."""
     unwanted_keys = ['vrerr6', 'vterr6']
-    for unwanted_key in unwanted_keys:
-        dictionary.pop(unwanted_key, None)
+    for key in dictionary.copy().keys():
+        if any(unwanted_key in key for unwanted_key in unwanted_keys):
+            dictionary.pop(key, None)
 
 
 def replace_key_substring(dictionary, old, new):
@@ -73,13 +76,19 @@ def substitute_files(dictionary, from_dir):
             for substitution in value:
                 sub_dict = read_in_json(os.path.join(
                     from_dir, substitution['file']))
-                remove_extra_keys(sub_dict)
                 if 'replace' in substitution:
                     text_to_replace = substitution['replace'][0]
                     text_to_use = substitution['replace'][1]
                     sub_dict = replace_key_substring(
                         sub_dict, text_to_replace, text_to_use)
+                if 'include' in substitution:
+                    new_sub_dict = {}
+                    for include in substitution['include']:
+                        for sub_key, sub_value in sub_dict[include].items():
+                            new_sub_dict[sub_key] = sub_value
+                    sub_dict = new_sub_dict
 
+                remove_extra_keys(sub_dict)
                 dictionary.update(sub_dict)
 
     return dictionary

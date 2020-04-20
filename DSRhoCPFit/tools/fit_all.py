d7ea45ceb1cb6dd7b7a6aff2f36031db795561ee
Daniel Cervenkov <d.cervenkov@gmail.com>
2020-04-20 19:47:35 +0200

    Format CPFit fit_all script

--- DSRhoCPFit/tools/fit_all.py
+++ DSRhoCPFit/tools/fit_all.py
@@ -7,9 +7,7 @@ import shutil
 import subprocess
 import sys
 
-DEFAULT_OPTIONS = ["--MC=1",
-                   "--cpus=1",
-                   "--log"]
+DEFAULT_OPTIONS = ["--MC=1", "--cpus=1", "--log"]
 COMMAND = "./DSRhoCPFit"
 CHANNELS = ["Kpi", "Kpipi0", "K3pi"]
 CONFIG_BASENAME = "config_stream"
@@ -17,26 +15,46 @@ RESULTS_DIR = "results"
 TEMP_FILE = "/tmp/temp_commands"
 
 CHANNELS_COMBINATIONS = CHANNELS[:]
-CHANNELS_COMBINATIONS += [",".join([c1, c2]) for c1 in CHANNELS for c2 in CHANNELS if c1 != c2]
+CHANNELS_COMBINATIONS += [
+    ",".join([c1, c2]) for c1 in CHANNELS for c2 in CHANNELS if c1 != c2
+]
 CHANNELS_COMBINATIONS += list(map(",".join, list(itertools.permutations(CHANNELS))))
+
 
 def decode_arguments():
     """Decode CLI arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--channel", action="append", help="channel to be processed",
-                        dest="channels", choices=CHANNELS_COMBINATIONS)
-    parser.add_argument("-d", "--td", action="store_true",
-                        help="do time-dependent fits")
-    parser.add_argument("-i", "--ti", action="store_true",
-                        help="do time-independent fits")
-    parser.add_argument("-m", "--component", dest="components", choices=["CR", "CRSCF", "all"],
-                        action="append", help="do CR-only fits")
-    parser.add_argument("-f", "--force", action="store_true",
-                        help="force rewrite of existing results")
     parser.add_argument(
-        "-j", "--cpus", help="number of cores to use", default=1, type=int)
-    parser.add_argument("-s", "--streams", default=6, type=int,
-                        help="force rewrite of existing results")
+        "-c",
+        "--channel",
+        action="append",
+        help="channel to be processed",
+        dest="channels",
+        choices=CHANNELS_COMBINATIONS,
+    )
+    parser.add_argument(
+        "-d", "--td", action="store_true", help="do time-dependent fits"
+    )
+    parser.add_argument(
+        "-i", "--ti", action="store_true", help="do time-independent fits"
+    )
+    parser.add_argument(
+        "-m",
+        "--component",
+        dest="components",
+        choices=["CR", "CRSCF", "all"],
+        action="append",
+        help="do CR-only fits",
+    )
+    parser.add_argument(
+        "-f", "--force", action="store_true", help="force rewrite of existing results"
+    )
+    parser.add_argument(
+        "-j", "--cpus", help="number of cores to use", default=1, type=int
+    )
+    parser.add_argument(
+        "-s", "--streams", default=6, type=int, help="force rewrite of existing results"
+    )
     args = parser.parse_args()
     return args
 
@@ -50,9 +68,9 @@ def create_fit_cmds(options):
     """Fit one channel with all the components, fit types, etc."""
     commands = []
     time_types = []
-    if (options.ti):
+    if options.ti:
         time_types.append("ti")
-    if (options.td):
+    if options.td:
         time_types.append("td")
 
     dirs_to_create = []
@@ -61,8 +79,10 @@ def create_fit_cmds(options):
     for time_type in time_types:
         for channel in options.channels:
             for component in options.components:
-                output_dir = os.path.join(RESULTS_DIR, "_".join(
-                    [channel.replace(",", "-"), time_type, component]))
+                output_dir = os.path.join(
+                    RESULTS_DIR,
+                    "_".join([channel.replace(",", "-"), time_type, component]),
+                )
                 if os.path.exists(os.path.join("..", output_dir)):
                     if options.force:
                         dirs_to_delete.append(output_dir)
@@ -77,14 +97,11 @@ def create_fit_cmds(options):
                     cmd.append(COMMAND)
                     cmd += DEFAULT_OPTIONS
                     cmd.append("--components=" + component)
-                    cmd.append("--config=" + CONFIG_BASENAME +
-                               str(stream) + ".json")
-                    excluded_channels = set(CHANNELS) - set(channel.split(','))
+                    cmd.append("--config=" + CONFIG_BASENAME + str(stream) + ".json")
+                    excluded_channels = set(CHANNELS) - set(channel.split(","))
                     if excluded_channels:
-                        cmd.append("--exclude-channels=" +
-                                   ",".join(excluded_channels))
-                    output_file = os.path.join(
-                        output_dir, "stream" + str(stream))
+                        cmd.append("--exclude-channels=" + ",".join(excluded_channels))
+                    output_file = os.path.join(output_dir, "stream" + str(stream))
                     cmd.append("--output=" + output_file)
                     if time_type == "ti":
                         cmd.append("--time-independent")
@@ -99,24 +116,23 @@ def main():
     if not are_valid(options):
         sys.exit(1)
 
-    commands, dirs_to_create, dirs_to_delete, dirs_to_skip = create_fit_cmds(
-        options)
+    commands, dirs_to_create, dirs_to_delete, dirs_to_skip = create_fit_cmds(options)
 
     print("Will run the following commands:")
     for command in commands:
         print(" ".join(command))
 
-    if (dirs_to_skip):
+    if dirs_to_skip:
         print("\nWill skip the following dirs:")
         for dir_to_skip in dirs_to_skip:
             print(dir_to_skip)
 
-    if (dirs_to_create):
+    if dirs_to_create:
         print("\nWill create the following dirs:")
         for dir_to_create in dirs_to_create:
             print(dir_to_create)
 
-    if (dirs_to_delete):
+    if dirs_to_delete:
         print("\nWill delete the following dirs:")
         for dir_to_delete in dirs_to_delete:
             print(dir_to_delete)
@@ -126,8 +142,8 @@ def main():
     print("Will process " + str(options.streams) + " stream(s)")
 
     user_input = input("\nContinue? [Y/n]: ")
-    if (user_input == "" or user_input.lower() == "y"):
-        with open(TEMP_FILE, 'w') as f:
+    if user_input == "" or user_input.lower() == "y":
+        with open(TEMP_FILE, "w") as f:
             for command in commands:
                 f.write(" ".join(command) + "\n")
 
@@ -138,7 +154,13 @@ def main():
             os.makedirs(os.path.join("..", dir))
 
         subprocess.run(
-            "parallel --will-cite --bar --nice 10 -j " + str(options.cpus) + " < " + TEMP_FILE, cwd="..", shell=True)
+            "parallel --will-cite --bar --nice 10 -j "
+            + str(options.cpus)
+            + " < "
+            + TEMP_FILE,
+            cwd="..",
+            shell=True,
+        )
         os.remove(TEMP_FILE)
     else:
         print("Aborting")
